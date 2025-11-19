c02a67f640c98a3f5d9af03d5bc4fa6bb7f838ba
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-19 11:21:01 +0530

    arrow function

---
+++ 3_JAVA_SCRIPT/2_Intermediate/2_arrow_function.js
@@ -0,0 +1,16 @@
+// Normal Function
+
+// function add(x,y){
+//     console.log(x+y)
+// }
+
+// add(10,20)
+
+// Arrow function
+
+const intro = (f_name, l_name = "Naik") => {
+  return `my name is : ${f_name} ${l_name}`;
+};
+
+details = intro("sanidhya");
+console.log(details);
