2700b13e3b84b500b6221b7f006b19abc84a0759
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-19 10:44:05 +0530

    Template Litrals

---
+++ 3_JAVA_SCRIPT/2_Intermediate/1_template_litrals.js
@@ -0,0 +1,20 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+let a = `The quick`;
+let b = `brown fox`;
+let c = `jumps over`;
+let d = `the lazy dog`;
+
+const firstName = "Sanidhya";
+const lastName = "Naik";
+
+console.log(`${a} ${b} ${c} ${d}`);
+
+console.log(`My name is : ${firstName} ${lastName}`);
