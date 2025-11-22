f30ce3a9a25a3c9d741b56f6c9a7afd1cc1e1618
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-22 19:36:54 +0530

    for of loop

---
+++ 3_JAVA_SCRIPT/2_Intermediate/13_for_of_loop.js
@@ -0,0 +1,13 @@
+// for of loop
+
+let Age = [12, 52, 49, 63, 48, 74];
+
+for (let value of Age) {
+  console.log(value);
+}
+
+let name = "Sanidhya";
+
+for (let char of name) {
+  console.log(char);
+}
---
+++ 3_JAVA_SCRIPT/2_Intermediate/14_foreach_hepler.js
@@ -0,0 +1,29 @@
+let words = ["sanidhya", "prem", "deepak", "digu", "tushar"];
+
+let capWords = words.forEach((words, index, arr) => {
+  //   console.log(words.toLocaleUpperCase());
+  arr[index] = words[0].toLocaleUpperCase() + words.substring(1);
+});
+console.log(words);
+
+// 1. Create an array of numbers.
+// 2. Create a variable to hold the sum.
+// 3. Create the adder function which takes the parameter of number & add that with sum variable.
+// 4. Loop over the array, incrementing the sum variable except this time a function gets referenced in the forEach
+// 5. print the sum variable.
+
+let numbers = [10, 25, 48, 96];
+
+let sum = 0;
+
+// function adder(number) {
+//   sum += number;
+// }
+// numbers.forEach(adder);
+
+numbers.forEach((number) => {
+  sum += number;
+});
+
+
+console.log(`Addition is ${sum}`);
