6b03f438a4e6ea3012ddf8c8d6406f867ec43faf
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-22 20:28:48 +0530

    find hepler

---
+++ 3_JAVA_SCRIPT/2_Intermediate/14_find_helper.js
@@ -0,0 +1,9 @@
+let products = [
+  { name: "Checkers", category: "Toys" },
+  { name: "Harry Potter", category: "Books" },
+  { name: "iPhone", category: "Electronics" },
+  { name: "Learn PHP", category: "Books" },
+];
+
+let match = products.find((product) => product.category == "Books");
+console.log(match);
