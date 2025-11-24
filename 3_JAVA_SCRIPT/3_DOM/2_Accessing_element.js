10afcd02fc4b0f590ff82126708cc149d623d49c
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-24 10:29:58 +0530

    DOM Selectors

---
+++ 3_JAVA_SCRIPT/3_DOM/2_Accessing_element.js
@@ -0,0 +1,22 @@
+// Get Element By TagName
+const h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+// Get Element By Id
+const id1 = document.getElementById("main");
+console.log(id1);
+
+// Get Element By Class
+const cls = document.getElementsByClassName("cls");
+console.log(cls);
+
+
+// Query Selector
+console.log(document.querySelector("h1"))
+console.log(document.querySelector("#main"))
+console.log(document.querySelector(".cls"))
+console.log(document.querySelector("li"))
+
+
+// Query Selector
+console.log(document.querySelectorAll("li"))
