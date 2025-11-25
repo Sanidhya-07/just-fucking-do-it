2764d9b3a4cc2d0c6868bc03a86a96199f423b53
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-25 07:53:08 +0530

    innertext innerHTML textcontent

---
+++ 3_JAVA_SCRIPT/3_DOM/3_innertext_textcontent_innerhtml.js
@@ -0,0 +1,11 @@
+// innerText
+const first = document.querySelector(".first");
+console.log(first.innerText);
+
+// innerHTML
+const second = document.querySelector(".second");
+console.log(first.innerHTML);
+
+// textContent
+const third = document.querySelector(".third");
+console.log(first.textContent);
