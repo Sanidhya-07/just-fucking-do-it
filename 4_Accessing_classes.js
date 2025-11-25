5c7fc4c0af581122f6fc6ace3433c8caa6ab1037
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-25 08:14:28 +0530

    classes

---
+++ 4_Accessing_classes.js
@@ -0,0 +1,13 @@
+// classlist -> Gives total numbers of classes applied
+const h1 = document.querySelector("h1");
+
+console.log(h1.classList);
+
+// Add class
+// h1.classList.add("bg");
+
+// Remove class
+// h1.classList.remove("bg");
+
+// toggle
+h1.classList.toggle("bg");
