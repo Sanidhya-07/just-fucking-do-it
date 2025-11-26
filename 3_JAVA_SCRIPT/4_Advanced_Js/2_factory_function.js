6db89c55bc11a8f009efce5aeb02e1b7e33f0695
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-11-26 16:31:53 +0530

    Factory function

---
+++ 3_JAVA_SCRIPT/4_Advanced_Js/2_factory_function.js
@@ -0,0 +1,23 @@
+// If a function return object then it is called factory function.
+
+// create a factory function that generates different types of vehicles.
+// Each vehicle object should have properties like type, brand, model, and year.
+
+function vehicle(type, brand, model, year) {
+  return {
+    Type: type,
+    Brand: brand,
+    Model: model,
+    Year: year,
+
+    Carinfo: function () {
+      return `My car Type is:${this.Type}\nMy car Brand is:${this.Brand}\nMy car Model is:${this.Model}\nMy car Year is:${this.Year}\n`;
+    },
+  };
+}
+
+const sunny = vehicle("Sedan", "Maruti", "SX", 2020);
+console.log(sunny.Carinfo());
+
+const prem = vehicle("hashback", "Toyoto", "top-model", 2023);
+console.log(prem.Carinfo());
