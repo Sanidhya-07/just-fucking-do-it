bb2fc98669b3af737a6d2dc56ef276699a850802
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-12-02 13:32:40 +0530

    access modifiers

---
+++ 3_JAVA_SCRIPT/4_Advanced_Js/7_Access_modifiers.js
@@ -0,0 +1,31 @@
+// Public, Private, Protected Example
+
+class Bank {
+  #pin; // private property
+
+  constructor(name, pin, balance) {
+    this.name = name; // public
+    this.#pin = pin; // private
+    this._balance = balance; // protected
+  }
+
+  showBalance(pin) {
+    if (pin === this.#pin) {
+      return this._balance;
+    } else {
+      return "Incorrect password";
+    }
+  }
+}
+
+class Saving extends Bank {
+  addInterest(amount) {
+    this._balance += amount; // allowed because _balance is protected
+  }
+}
+
+const acc = new Saving("Sanidhya", 4567, 2000);
+
+console.log(acc.showBalance(4567)); // 2000
+acc.addInterest(500);
+console.log(acc.showBalance(4567)); // 2100
