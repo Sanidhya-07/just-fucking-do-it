695b288492d231310ca1154340ebbaeed066f572
Sanidhya Naik <sanidhya.dev7@gmail.com>
2025-12-07 10:04:57 +0530

    callback hell

---
+++ 3_JAVA_SCRIPT/5_Advanced_JS ( Async js )/1_call_back_hell.js
@@ -0,0 +1,63 @@
+orderDetails = {
+  orderId: 123654,
+  food: ["pizza", "burger", "coke"],
+  cost: 650,
+  customer_name: "sanidhya naik",
+  address: "pune",
+  restaurant_location: "mumbai",
+};
+
+const { orderId, food, cost, customer_name, address, restaurant_location } =
+  orderDetails;
+
+function placeOrder(orderDetails, callback) {
+  console.log(`Payment of ${cost} is processing...`);
+
+  setTimeout(() => {
+    console.log(`Payment is completed and order get placed !`);
+    callback(orderDetails);
+  }, 2000);
+}
+
+function preparingFood(orderDetails, callback) {
+  console.log(`Your food preparation of ${food} is started.`);
+
+  setTimeout(() => {
+    console.log(`Your order is now prepared.`);
+    callback(orderDetails);
+  }, 4000);
+}
+
+function pickupOrder(orderDetails, callback) {
+  console.log(
+    `Delivery boy is on its way to ${restaurant_location} to pickup your order.`
+  );
+
+  setTimeout(() => {
+    console.log(
+      `I have picked up your order and i am on my way to ${address} deliver it.`
+    );
+    callback(orderDetails);
+  }, 5000);
+}
+
+function deliverOrder() {
+  setTimeout(() => {
+    console.log(`Delivery boy reached ${address}.`);
+  }, 2000);
+
+  setTimeout(() => {
+    console.log(`order is delivered successfully.`);
+    console.log(`Thank you ${customer_name} order again.`);
+  }, 4000);
+}
+
+// callback hell ...
+
+placeOrder(orderDetails, (orderDetails) => {
+  preparingFood(orderDetails, (orderDetails) => {
+    pickupOrder(orderDetails, () => {
+      deliverOrder();
+    });
+  });
+});
